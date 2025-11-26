3b0c2dbfe72eaef5a038fb95d4ae96da0548b2e1
Alex Staicu <alexstaicu628@gmail.com>
2025-11-26 22:05:23 +0000

    basic webhook inspector functionality

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
+webhooks = []
 
-@app.get("/")
-def home():
-    return {"message": "Webhook Inspector is running."}
+
+@app.post("/receive")
+async def receive_webhook(request: Request):
+    body = await request.body()
+
+    event = {
+        "body": body.decode(),
+        "headers": dict(request.headers),
+        "query_params": dict(request.query_params),
+    }
+
+    webhooks.append(event)
+    return {"status": "ok"}
+
+
+@app.get("/webhooks")
+def get_events():
+    return webhooks
