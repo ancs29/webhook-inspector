50c41ccb140063531927082adc9b53f4178a8e4f
Alex Staicu <alexstaicu628@gmail.com>
2025-12-04 17:12:44 +0000

    empty

--- main.py
+++ main.py
@@ -38,3 +38,6 @@ def get_webhooks():
 @app.exception_handler(Exception)
 async def global_exception_handler(_request: Request, _exc: Exception):
     return JSONResponse(status_code=500, content={"detail": "Internal server error"})
+
+
+# code commit
